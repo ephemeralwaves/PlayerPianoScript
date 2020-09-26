2e8b470db6a7014b32f2b793d4e978fb6e7589e9
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2020-09-26 08:53:59 -0400

    Port no longer hardcoded

--- pmidihi.sh
+++ pmidihi.sh
@@ -3,15 +3,21 @@
 #Note check port with(as sometimes it changes):
 #aplaymidi -l
 
+#enter: crontab -e 
+#and put in following:
+#SHELL=/bin/bash
+#00 8 * * * /bin/bash /home/ebonyivorystudio/Music/PlayerPianoScript/pmidihi.sh $
+
 #go to dir with songs
-cd /home/ebonyivorystudio/Music/Music/Midi/AmSongs
+cd /home/ebonyivory/Nextcloud/Music/Midi/AmSongs
 
 #take 10 random files and assign to songarray
 songsArray=($(ls | shuf -n 10))
+port=$(aplaymidi -l | grep UM-ONE)
 
 #play songs
 for i in "${songsArray[@]}"
 do
-	pmidi -p "20:0" $i
+	pmidi -p ${port:0:5} $i
 	wait
 done
